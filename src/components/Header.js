0506d6b7d924f61b0f3cddcf5a13c89395916d2f
IgorDedul <dedul_igor@mail.ru>
2023-06-21 00:27:03 +0300

    feat: create base structure

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import logoHeader from '../images/logo-header.svg';
+
+const Header = () => {
+  return (
+      <header className="header page__header">
+        <img
+          className="header__logo"
+          src={logoHeader}
+          alt="Логотип - надпись Mesto"
+        />
+      </header>
+  );
+};
+
+export default Header;
