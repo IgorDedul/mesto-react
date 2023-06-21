70716e0f42678dde131947332f40922b650393b3
IgorDedul <dedul_igor@mail.ru>
2023-06-21 23:37:01 +0300

    feat: create popupwithform and popupimage

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const PopupWithForm = (props) => {
   return (
-    <div className={`popup popup_type_${props.name} ${props.isOpen ? "popup_opened" : ""}`}>
+    <div className={`popup popup_${props.name} ${props.isOpen ? "popup_opened" : ""}`}>
       <div className="popup__container">
-        <button type="button" className="popup__close" onClick={props.onClose}></button>
+        <button type="button" className="popup__close-button" onClick={props.onClose}></button>
         <h3 className="popup__title">{props.title}</h3>
         <form
           name={`${props.name}`}
